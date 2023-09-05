1916ee41f76fcf9058917468f5b784ca206df2ee
Massan0917 <massan000917@gmail.com>
2023-09-05 05:55:31 +0000

    make do bot

--- chatbot.py
+++ chatbot.py
@@ -7,26 +7,25 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 json_open = open('tokens.json', 'r')
 tokens = json.load(json_open)
-print(tokens['SLACK_BOT_TOKEN'])
 
-# SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-# SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+SLACK_BOT_TOKEN = tokens['SLACK_BOT_TOKEN']
+SLACK_APP_TOKEN = tokens['SLACK_APP_TOKEN']
 # print(SLACK_BOT_TOKEN)
 # print(SLACK_APP_TOKEN)
 
-# app = App(token=SLACK_BOT_TOKEN)
+app = App(token=SLACK_BOT_TOKEN)
 
-# @app.message("hello")  # 送信されたメッセージ内に"hello"が含まれていたときのハンドラ
-# def ask_who(say):
-#     say("can I help you?")
+@app.message("hello")  # 送信されたメッセージ内に"hello"が含まれていたときのハンドラ
+def ask_who(say):
+    say("can I help you?")
 
-# @app.event("app_mention")  # chatbotにメンションが付けられたときのハンドラ
-# def respond_to_mention(event, say):
-#     message = re.sub(r'^<.*>', '', event['text'])
-#     say(message[::-1]) # 文字列を逆順
+@app.event("app_mention")  # chatbotにメンションが付けられたときのハンドラ
+def respond_to_mention(event, say):
+    message = re.sub(r'^<.*>', '', event['text'])
+    say(message[::-1]) # 文字列を逆順
 
-# @app.event("message") # ロギング
-# def handle_message_events(body, logger):
-#     logger.info(body)
+@app.event("message") # ロギング
+def handle_message_events(body, logger):
+    logger.info(body)
 
-# SocketModeHandler(app, SLACK_APP_TOKEN).start()
+SocketModeHandler(app, SLACK_APP_TOKEN).start()
