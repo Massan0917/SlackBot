926fa167000ed1d40f28964b2dfd74312cc528f7
Massan0917 <massan000917@gmail.com>
2023-09-27 07:58:37 +0000

    masui_dev_DetectFiletype

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,10 @@
 import os
 import re
 import json
+import requests
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from datetime import datetime
 
 import push_to_git as ptg
 
@@ -17,8 +19,9 @@ SLACK_APP_TOKEN = tokens['SLACK_APP_TOKEN']
 app = App(token=SLACK_BOT_TOKEN)
 
 @app.message("hello")  # 送信されたメッセージ内に"hello"が含まれていたときのハンドラ
-def ask_who(say):
+def ask_who(event,say):
     say("can I help you?")
+    say(event['text'])
 
 @app.event("app_mention")  # chatbotにメンションが付けられたときのハンドラ
 def push_to_git(event, say):
@@ -29,6 +32,48 @@ def push_to_git(event, say):
     ptg.push(splited_message[0] ,splited_message[1])
     say('done!')
     say('pushed at https://github.com/Massan0917/SlackBot')
+
+@app.event({"type": "message", "subtype": "file_share"})
+def file_share(event, say):
+    say(event['files'][0]['filetype'])
+    say(event['files'][0]['name'])
+
+    if event["files"][0]["filetype"] in["png", "jpg"]:
+        path='img/'
+    elif event["files"][0]["filetype"] == "markdown":
+        path='news/'
+    else:
+        path=''
+
+    # スレッドからファイルをダウンロード
+    download_from_slack(
+        download_url = event["files"][0]["url_private_download"], 
+        auth = SLACK_BOT_TOKEN,
+        path = path,
+        filename = event['files'][0]['name']
+    )
+
+def download_from_slack(download_url: str, auth: str, path:str, filename:str):
+    """Slackから画像をダウンロードして保存し、保存したパスを返す。
+
+    Args:
+        download_url (str): 画像のURL
+        auth (str): 画像の閲覧に必要なSlackの認証キー
+
+    Returns:
+        str: 画像が保存されているパス
+    """
+    img_data = requests.get(
+        download_url,
+        allow_redirects=True,
+        headers={"Authorization": f"Bearer {auth}"},
+        stream=True,
+    ).content
+
+    filename = path + filename
+    with open(filename, "wb") as f:
+        f.write(img_data)
+
 
 @app.event("message") # ロギング
 def handle_message_events(body, logger):
