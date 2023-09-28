3023e27b61e9dece592945379ffadefa72c28345
Massan0917 <massan000917@gmail.com>
2023-09-28 07:01:51 +0000

    masui_refactoring chatbot.py, push_to_git.py

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,9 @@
-import os
-import re
+# Author: Masui Taichi
+
 import json
 import requests
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from datetime import datetime
 
 import push_to_git as ptg
 
@@ -13,31 +12,29 @@ tokens = json.load(json_open)
 
 SLACK_BOT_TOKEN = tokens['SLACK_BOT_TOKEN']
 SLACK_APP_TOKEN = tokens['SLACK_APP_TOKEN']
-# print(SLACK_BOT_TOKEN)
-# print(SLACK_APP_TOKEN)
 
 app = App(token=SLACK_BOT_TOKEN)
 
-@app.message("hello")  # 送信されたメッセージ内に"hello"が含まれていたときのハンドラ
-def ask_who(event,say):
-    say("can I help you?")
-    say(event['text'])
-
-@app.event("app_mention")  # chatbotにメンションが付けられたときのハンドラ
-def push_to_git(event, say):
-    say('pushing to github...')
-    message = re.sub(r'^<.*>', '', event['text']) # メンションを削除
-    splited_message = message.split('\n')
-    print(splited_message)
-    ptg.push(splited_message[0] ,splited_message[1])
-    say('done!')
-    say('pushed at https://github.com/Massan0917/SlackBot')
-
-@app.event({"type": "message", "subtype": "file_share"})
+# when app is mentioned
+@app.event("app_mention") 
 def file_share(event, say):
+    """
+    download file from slack and push to github
+
+    Parameters
+    ----------
+    event : dict
+        event data from slack
+    say : function
+        function to say
+
+    """
     say(event['files'][0]['filetype'])
     say(event['files'][0]['name'])
 
+    # get filename
+    filename = event['files'][0]['name']
+    # check filetype and set path
     if event["files"][0]["filetype"] in["png", "jpg"]:
         path='img/'
     elif event["files"][0]["filetype"] == "markdown":
@@ -45,24 +42,34 @@ def file_share(event, say):
     else:
         path=''
 
-    # スレッドからファイルをダウンロード
+    # download file from slack
     download_from_slack(
         download_url = event["files"][0]["url_private_download"], 
         auth = SLACK_BOT_TOKEN,
         path = path,
-        filename = event['files'][0]['name']
+        filename = filename
     )
 
+    # push to github
+    push_to_git(filepath=path + filename , say=say)
+
 def download_from_slack(download_url: str, auth: str, path:str, filename:str):
-    """Slackから画像をダウンロードして保存し、保存したパスを返す。
-
-    Args:
-        download_url (str): 画像のURL
-        auth (str): 画像の閲覧に必要なSlackの認証キー
-
-    Returns:
-        str: 画像が保存されているパス
     """
+    download file from slack
+
+    Parameters
+    ----------
+        download_url : str
+            URL to download
+        auth : str
+            Slack Bot Token
+        path : str
+            path to save
+        filename : str
+            filename to save
+    """
+
+    # download file
     img_data = requests.get(
         download_url,
         allow_redirects=True,
@@ -70,12 +77,33 @@ def download_from_slack(download_url: str, auth: str, path:str, filename:str):
         stream=True,
     ).content
 
+    # save file
     filename = path + filename
     with open(filename, "wb") as f:
         f.write(img_data)
 
+def push_to_git(filepath:str, say):
+    """
+    push file to github
 
-@app.event("message") # ロギング
+    Parameters
+    ----------
+        filepath : str
+            path to file
+        say : function
+            function to say
+    """
+
+    # this link is just say to user
+    # repository to push depends on running environment
+    link_to_github = "https://github.com/NAIST-SE/naist-se.github.io/"
+    say('pushing to github...')
+    ptg.push(filepath=filepath)
+    say('done!')
+    say(f'pushed at {link_to_github}')
+
+
+@app.event("message") # logging
 def handle_message_events(body, logger):
     logger.info(body)
 
